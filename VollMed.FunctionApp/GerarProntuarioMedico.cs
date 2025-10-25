379b192aa4839652371b9754ee3547363319d249
marcelooliveira <mclricardo@gmail.com>
2025-10-25 10:05:42 -0300

    Nova função GerarProntuarioMedicoV4

--- VollMed.FunctionApp/GerarProntuarioMedico.cs
+++ VollMed.FunctionApp/GerarProntuarioMedico.cs
@@ -14,7 +14,7 @@ public class GerarProntuarioMedico
         _logger = logger;
     }
 
-    [Function("GerarProntuarioMedicoV3")]
+    [Function("GerarProntuarioMedicoV4")]
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
