c60b1c3fe64b95e93aed9aee7e6055f5f861c91e
marcelooliveira <mclricardo@gmail.com>
2025-10-25 08:47:34 -0300

    Nova função GerarProntuarioMedico

---
+++ VollMed.FunctionApp/GerarProntuarioMedico.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace VollMed.FunctionApp;
+
+public class GerarProntuarioMedico
+{
+    private readonly ILogger<GerarProntuarioMedico> _logger;
+
+    public GerarProntuarioMedico(ILogger<GerarProntuarioMedico> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("GerarProntuarioMedico")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
